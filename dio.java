93603a6d866de50c8b2235cae59de94658fe5317
daniela marques <dd978452@gmail.com>
2023-01-19 11:27:01 -0300

    meu primeiro sistema em java

---
+++ dio.java
@@ -0,0 +1,22 @@
+//ternário
+
+public class dio{ 
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+    
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        /*if(a==b)
+            resultado = "verdadeiro";
+        else 
+            resultado = "falso";
+        */
+        
+        System.out.println(resultado);
+       
+    }
+
+}
